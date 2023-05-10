5f7e965cb17cad091ef530735ccba56545602894
swethabikkasani <43268501+swethabikkasani@users.noreply.github.com>
2023-05-10 20:37:12 +0530

    Update HelloWorldTest.scala

--- src/test/scala/org/jacoco/examples/maven/scala/HelloWorldTest.scala
+++ src/test/scala/org/jacoco/examples/maven/scala/HelloWorldTest.scala
@@ -1,13 +1,1 @@
-package org.jacoco.examples.maven.scala
 
-import org.junit._
-import Assert._
-
-@Test
-class HelloWorldTest {
-
-    @Test
-    def testMessage() = {
-    	assertEquals("Hello World!", HelloWorld.message(false))  
-	}
-}
